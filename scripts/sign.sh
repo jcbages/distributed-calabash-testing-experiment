10438af652af4004af9da3a69f7c173215e1d481
Juan Camilo Bages <jcbages95@gmail.com>
2018-12-08 17:31:30 -0500

    scripts ready

---
+++ scripts/sign.sh
@@ -0,0 +1,15 @@
+# USE FROM INSIDE MUTANTS FOLDER
+
+for folder in */ ; do
+    # get in folder
+    cd $folder
+
+    # sign apk
+    jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/.android/debug.keystore mileage.apk androiddebugkey -storepass android
+
+    # print message
+    echo "Done signing $folder"
+
+    # go back again
+    cd ..
+done
