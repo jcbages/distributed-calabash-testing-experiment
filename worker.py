4548e65873d84b07a00d47de81f6c282cf0030db
Juan Camilo Bages <jcbages95@gmail.com>
2018-12-08 22:21:16 -0500

    fix scripts

--- worker.py
+++ worker.py
@@ -10,11 +10,11 @@ os.system('rm status.txt')
 worker_file = sys.argv[1]
 with open('workers/%s' % worker_file) as worker:
     for mutant in worker:
-        os.system('./run.sh %s >> output.txt 2>&1 # run the test for it' % mutant)
-        os.system('echo " [x] Finished %s" >> status.txt' % mutant)
+        os.system('./run.sh %s' % mutant)
+        os.system('echo " [x] Wow! Finished %s" >> status.txt' % mutant)
 
 # upload results
 os.system('echo " [x] Uploading all results"')
-os.system('./upload.sh $1 >> output.txt 2>&1')
+os.system('./upload.sh %s' % worker_file)
 
-os.system('echo " [x] Finished all!" >> status.txt')
+os.system('echo " [x] Wow! Finished all!" >> status.txt')
